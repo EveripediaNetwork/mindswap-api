b874d570d753c577e940884b2af44bf36693d482
Cesar Rodriguez <kesarr@gmail.com>
2020-12-30 13:57:05 +0100

    refactorings

--- api/pools.js
+++ api/pools.js
@@ -1,16 +1,34 @@
 require('dotenv').config()
+global.fetch = require("node-fetch");
+global.WebSocket = require("ws");
+const {createDfuseClient} = require("@dfuse/client")
 
 module.exports = async (req, res) => {
-    res.setHeader('Cache-Control', 'max-age=0, s-maxage=86400')
-    if (!req.query.account) {
-        res.status(403).send('ERROR');
-    }
+    res.setHeader('Cache-Control', 'max-age=0, s-maxage=600')
+    const client = createDfuseClient({
+        authentication: false,
+        network: 'eos.dfuse.eosnation.io' // 'kylin.dfuse.eosnation.io'
+    });
 
     try {
-        // TODO: something
-        res.status(200).send(`OK`);
-    } catch (e) {
-        console.log('\nCaught exception: ' + e)
+        let pools = [];
+        const states = await client.stateTableScopes("mindswapswap", "stat");
+        const tables = await client.stateTablesForScopes("mindswapswap", states.scopes, "stat", {
+            json: true,
+            keyType: "symbol_code"
+        });
+        client.release();
+
+        tables.tables.forEach((table) => {
+            pools.push(table.rows[0]);
+        })
+
+        res.status(200).send({
+            pools
+        });
+
+    } catch (error) {
+        console.error("An error occurred", error)
         res.status(500).send('ERROR 500');
     }
 }
--- api/userPools.js
+++ api/userPools.js
@@ -2,92 +2,15 @@ require('dotenv').config()
 global.fetch = require("node-fetch");
 global.WebSocket = require("ws");
 const {createDfuseClient} = require("@dfuse/client")
-
-let balances = [
-    {
-        name: "IQYDFS",
-        precision: 3,
-        contract: "mindswapswap",
-        balance: 0,
-        icon: "tokens/yesdfs.png",
-    },
-    {
-        name: "IQNDFS",
-        precision: 3,
-        contract: "mindswapswap",
-        balance: 0,
-        icon: "tokens/nodfs.png",
-    },
-    {
-        name: "IQYUPB",
-        precision: 3,
-        contract: "mindswapswap",
-        balance: 0,
-        icon: "tokens/yeskrw.png",
-    },
-    {
-        name: "IQNUPB",
-        precision: 3,
-        contract: "mindswapswap",
-        balance: 0,
-        icon: "tokens/nokrw.png",
-    },
-    {
-        name: "IQYTRMP",
-        precision: 3,
-        contract: "mindswapswap",
-        balance: 0,
-        icon: "tokens/yestrump.png",
-    },
-    {
-        name: "IQNTRMP",
-        precision: 3,
-        contract: "mindswapswap",
-        balance: 0,
-        icon: "tokens/notrump.png",
-    },
-    {
-        name: "IQYVAC",
-        precision: 3,
-        contract: "mindswapswap",
-        balance: 0,
-        icon: "tokens/yvac.png",
-    },
-    {
-        name: "IQNVAC",
-        precision: 3,
-        contract: "mindswapswap",
-        balance: 0,
-        icon: "tokens/nvac.png",
-    },
-    {
-        name: "EOSIQ",
-        precision: 4,
-        contract: "mindswapswap",
-        balance: 0,
-        icon: "tokens/eos.png",
-    },
-    {
-        name: "IQYBID",
-        precision: 3,
-        contract: "mindswapswap",
-        balance: 0,
-        icon: "tokens/yebiden.png",
-    },
-    {
-        name: "IQNBID",
-        precision: 3,
-        contract: "mindswapswap",
-        balance: 0,
-        icon: "tokens/nobiden.png",
-    },
-];
+let {balances} = require("./src/constants");
 
 module.exports = async (req, res) => {
     // res.setHeader('Cache-Control', 'max-age=0, s-maxage=86400')
     if (!req.query.account) {
         return res.status(403).send('ERROR');
     }
+
+    let poolBalances = balances.filter(balance => balance.contract === "mindswapswap");
 
     const client = createDfuseClient({
         authentication: false,
@@ -114,9 +37,6 @@ module.exports = async (req, res) => {
       trace {
         status
         matchingActions {
-          authorization {
-            actor
-          }
           name
           json
           dbOps {
@@ -136,6 +56,7 @@ module.exports = async (req, res) => {
 `
 
     try {
+        let pools = [];
         const response = await client.graphql(historyQuery, {
             variables: {
                 account: req.query.account,
@@ -144,14 +65,19 @@ module.exports = async (req, res) => {
                 "query": "account:mindswapswap (action:addliquidity OR action:remliquidity) auth:" + req.query.account
             },
         })
-
         const states = await client.stateTableScopes("mindswapswap", "stat");
-        let pools = [];
-        const tables = await client.stateTablesForScopes("mindswapswap", states.scopes, "stat", { json: true, keyType: "symbol_code" } );
-        console.log(tables);
+        const tables = await client.stateTablesForScopes("mindswapswap", states.scopes, "stat", {
+            json: true,
+            keyType: "symbol_code"
+        });
+        client.release();
+
+        tables.tables.forEach((table) => {
+            pools.push(table.rows[0]);
+        })
 
         response.data.accountBalances.edges.forEach((balance) => {
-            balances.forEach(initialBalance => {
+            poolBalances.forEach(initialBalance => {
                 if (
                     balance &&
                     balance.node &&
@@ -164,16 +90,14 @@ module.exports = async (req, res) => {
             });
         })
 
-        client.release()
-
         res.status(200).send({
-            pools: tables,
-            current: balances,
-            history: response.data.searchTransactionsBackward.results
+            pools: pools,
+            current: poolBalances,
+            history: (req.query.hideHistory && req.query.hideHistory === "true") ? null : response.data.searchTransactionsBackward.results
         });
 
     } catch (error) {
-        console.log("An error occurred", error)
+        console.error("An error occurred", error)
         res.status(500).send('ERROR 500');
     }
 }
