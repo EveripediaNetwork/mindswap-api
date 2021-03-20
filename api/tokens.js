258024adaa82661023df80806e4e5da7f897c6bf
epg323 <epg323@ucla.edu>
2021-03-20 07:37:15 -0700

    test

--- api/tokens.js
+++ api/tokens.js
@@ -3,7 +3,7 @@ const {tokens} = require("./src/constants");
 
 module.exports = async (req, res) => {
     try {
-        res.setHeader('Cache-Control: s-maxage=1, stale-while-revalidate');
+        res.setHeader('Cache-Control', 'Cache-Control: s-maxage=1, stale-while-revalidate');
         res.status(200).send(tokens);
     } catch (error) {
         console.error("An error occurred", error)
