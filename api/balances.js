a20b6d36bc155482f86f1c15e569e3f0caae7640
epg323 <epg323@ucla.edu>
2021-03-20 15:37:43 -0700

    remove headers

--- api/balances.js
+++ api/balances.js
@@ -58,7 +58,7 @@ module.exports = async (req, res) => {
 
         client.release()
 
-        res.setHeader('Cache-Control', 'Cache-Control: s-maxage=1, stale-while-revalidate');
+        res.setHeader('Cache-Control', 'no-cache');
         res.status(200).send(balances);
     } catch (error) {
         console.error("An error occurred", error)
