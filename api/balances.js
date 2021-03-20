80acb0b90a1e067100cd9117859f5f022bf38866
epg323 <epg323@ucla.edu>
2021-03-20 11:46:30 -0700

    test 3

--- api/balances.js
+++ api/balances.js
@@ -58,6 +58,7 @@ module.exports = async (req, res) => {
 
         client.release()
 
+        res.setHeader('Cache-Control', 'Cache-Control: no-cache, no-store, s-maxage=1, stale-while-revalidate');
         res.status(200).send(balances);
     } catch (error) {
         console.error("An error occurred", error)
