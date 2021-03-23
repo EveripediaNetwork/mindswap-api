8693d73d482d435050ee939cfbac77e1ba20e0c0
César Rodríguez <kesarr@gmail.com>
2021-03-23 20:43:53 +0100

    Update iq.js

--- api/iq.js
+++ api/iq.js
@@ -3,6 +3,6 @@ global.fetch = require('node-fetch')
 
 module.exports = async (req, res) => {
   const data = await fetch("https://www.api.bloks.io/tokens/IQ-eos-everipediaiq");
-  const result = data.json();
+  const result = await data.json();
   res.status(200).send(result[0].supply.circulating)
 }
